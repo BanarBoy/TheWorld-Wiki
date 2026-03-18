c32d51a66b09b0a48242888dae998f02eb4cc659
Kevin McCullough <kevinwmccullough@gmail.com>
2026-03-18 11:52:16 -0400

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "banarboy.github.io/TheWorld-Wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
